94591792cb2d7b4e497a287b4babeacb8f8db125
zhouhaijie <zhouhaijie2018@163.com>
2021-10-10 15:09:42 +0800

    master first commit by idea

--- src/main/java/com/zhouhai/hiveUdf/hiveTest4.java
+++ src/main/java/com/zhouhai/hiveUdf/hiveTest4.java
@@ -12,5 +12,6 @@ public class hiveTest4 {
         System.out.println("ni");
         System.out.println("flink");
         System.out.println("spark");
+        System.out.println("hive");
     }
 }
